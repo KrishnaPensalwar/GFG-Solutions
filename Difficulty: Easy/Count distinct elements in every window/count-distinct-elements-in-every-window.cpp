0bcc2b9dfbfd47dbf97884271d790a7e09c70a76
Krishna Pensalwar <100767867+KrishnaPensalwar@users.noreply.github.com>
2025-01-10 10:41:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count distinct elements in every window/count-distinct-elements-in-every-window.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> countDistinct(vector<int> &arr, int k) {
+        // code here.
+        
+        vector<int> ans;
+        
+        // for(int i=0;i<=arr.size()-k;i++){
+        //     set<int> st;
+            
+        //     for(int j=i;j<i+k;j++){
+        //         st.insert(arr[j]);
+        //     }
+        //     ans.push_back(st.size());
+        // }
+        
+        unordered_map<int,int> mp;
+        for(int i=0;i<k;i++){
+            mp[arr[i]]++;
+        }
+        
+        // cout<<mp.size()<<endl;
+        
+        ans.push_back(mp.size());
+        int j=0;
+        
+        for(int i=k;i<arr.size();i++){
+            mp[arr[j]]--;
+            if(mp[arr[j]]==0){
+                mp.erase(arr[j]);
+            }
+            j++;
+            mp[arr[i]]++;
+            ans.push_back(mp.size());
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution obj;
+        vector<int> res = obj.countDistinct(arr, k);
+        for (auto it : res)
+            cout << it << " ";
+        cout << endl;
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
